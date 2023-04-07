27235836bd27fc9a6c01de5265a70c0bc4ba72ce
Koen van Kruining <kvkruining@sogyo.nl>
2023-04-07 16:36:07 +0200

    Maak Putjeplunderen privé.

--- domain/src/main/java/mancala/domain/bakken/Put.java
+++ domain/src/main/java/mancala/domain/bakken/Put.java
@@ -49,7 +49,7 @@ public class Put extends Bak{
     }
     void ballenNaarKalaha(int aantalBallen) {buurBak.ballenNaarKalaha(aantalBallen);}
 
-    void putjePlunderen(){
+    private void putjePlunderen(){
         ballen+= overbuurPut.vraagAantalBallenOp();
         overbuurPut.ballen=0;
         ballenNaarKalaha();
