cab93728ce2ec52b48d1ac593a6b8c702d3c3f70
Koen van Kruining <kvkruining@sogyo.nl>
2023-04-07 15:34:01 +0200

    Voek tests toe om te checken dat overbuurbakken verschillende eigenaars hebben.

--- domain/src/test/java/mancala/domain/bakken/PutTest.java
+++ domain/src/test/java/mancala/domain/bakken/PutTest.java
@@ -1,8 +1,10 @@
 package mancala.domain.bakken;
 
-import mancala.domain.Eigenaar;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import mancala.domain.Eigenaar;
+import static mancala.domain.Spelmaker.maakBord;
 
 public class PutTest{
     @Test
@@ -11,5 +13,12 @@ public class PutTest{
         Put put=new Put(eigenaar);
 
         Assertions.assertEquals(4,put.vraagAantalBallenOp());
+    }
+
+    @Test
+    public void testOverbuurputtenHebbenVerschillendeEigenaars(){
+        Bak[] speelbord=maakBord(1);
+
+        Assertions.assertNotEquals(speelbord[0].eigenaar,speelbord[2].eigenaar);
     }
 }
