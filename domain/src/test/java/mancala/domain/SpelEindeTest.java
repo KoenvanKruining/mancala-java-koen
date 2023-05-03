9052011720d801b706f615f9b0198e6acfa90f5e
Koen van Kruining <kvkruining@sogyo.nl>
2023-05-03 14:54:28 +0200

    Test bepaling van winnaar.

--- domain/src/test/java/mancala/domain/SpelEindeTest.java
+++ domain/src/test/java/mancala/domain/SpelEindeTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import mancala.domain.bakken.Bak;
 import mancala.domain.bakken.Put;
+import static mancala.domain.Playable.Winner.*;
 
 import static mancala.domain.Spelmaker.maakBord;
 
@@ -20,5 +21,51 @@ public class SpelEindeTest {
         Assertions.assertEquals(3,spelbord[1].vraagAantalBallenOp());
         Assertions.assertEquals(0,spelbord[2].vraagAantalBallenOp());
         Assertions.assertEquals(5,spelbord[3].vraagAantalBallenOp());
+    }
+
+    @Test
+    public void testSpelStoptEnBepaaltWinnaar1(){
+        Playable spel= new MancalaSpel("Rick", "Viviyan",1);
+        spel.playPit(0);
+        spel.playPit(0);
+
+        Assertions.assertTrue(spel.isEndOfGame());
+        Assertions.assertEquals(PLAYER_2,spel.getWinner());
+    }
+
+    @Test
+    public void testSpelStoptEnBepaaltWinnaar2(){
+        Playable spel= new MancalaSpel("Rick", "Viviyan",2,1);
+        spel.playPit(0);
+        spel.playPit(4);
+        spel.playPit(3);
+        spel.playPit(1);
+        spel.playPit(4);
+        spel.playPit(3);
+
+        Assertions.assertTrue(spel.isEndOfGame());
+        Assertions.assertEquals(PLAYER_2,spel.getWinner());
+    }
+    @Test
+    public void testSpelStoptEnBepaaltGelijkspel(){
+        Playable spel= new MancalaSpel("Rick", "Viviyan",1,1);
+        spel.playPit(0);
+
+        Assertions.assertTrue(spel.isEndOfGame());
+        Assertions.assertEquals(DRAW,spel.getWinner());
+    }
+
+    @Test
+    public void testSpelStoptEnBepaaltGelijkspel2(){
+        Playable spel= new MancalaSpel("Rick", "Viviyan",2,2);
+        spel.playPit(0);
+        spel.playPit(1);
+        spel.playPit(3);
+        spel.playPit(0);
+        spel.playPit(4);
+        spel.playPit(0);
+
+        Assertions.assertTrue(spel.isEndOfGame());
+        Assertions.assertEquals(DRAW,spel.getWinner());
     }
 }
