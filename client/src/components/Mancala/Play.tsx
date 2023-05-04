2aafbb5ff12e27e7095d7ea441d298ed199dbdc2
Koen van Kruining <kvkruining@sogyo.nl>
2023-05-04 14:08:08 +0200

    Start frontend

--- client/src/components/Mancala/Play.tsx
+++ client/src/components/Mancala/Play.tsx
@@ -1,4 +1,4 @@
-import { GameState } from "../../types/gameState";
+import { GameState, Pit, Player } from "../../types/gameState";
 import "./Play.css";
 
 type PlayProps = {
@@ -6,13 +6,26 @@ type PlayProps = {
   setGameState(newGameState: GameState): void;
 };
 
+function Bak(props: {pit: Pit, player: Player}){
+  if(props.pit.index %props.player.pits.length==props.player.pits.length-1){return(
+    <div>{props.pit.nrOfStones}</div>
+  )} else if(props.player.hasTurn && props.pit.nrOfStones!=0){ return(
+    <div>{props.pit.nrOfStones}
+    <button>Play pit</button></div>
+  )} else {return(
+    <div>{props.pit.nrOfStones}</div>
+  )}
+  
+}
+
 export function Play({ gameState, setGameState }: PlayProps) {
   return (
     <div>
       <p>
         {gameState.players[0].name} vs {gameState.players[1].name}
       </p>
-      To do...
+      <div className="side">{gameState.players[0].pits.map((pit)=><Bak pit={pit} player={gameState.players[0]}></Bak>)}</div>
+      <div className="side">{gameState.players[1].pits.map((pit)=><Bak pit={pit} player={gameState.players[1]}></Bak>)}</div>
     </div>
-  );
+  )
 }
